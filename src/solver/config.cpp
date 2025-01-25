b37ba5335f3e34e1ef54a9f17ab7395efbfed33f
Dmitry Shustrov <45311367+shustrov38@users.noreply.github.com>
2025-01-25 23:29:06 +0300

    simple pass rule implementation (#8)

--- src/solver/config.cpp
+++ src/solver/config.cpp
@@ -18,12 +18,16 @@ std::istream &operator>>(std::istream& in, Config& config)
         config = DSATUR_FIBONACCI_HEAP;
     } else if (token == "DSATUR_SEWELL") {
         config = DSATUR_SEWELL;
+    } else if (token == "DSATUR_PASS") {
+        config = DSATUR_PASS;
 
     } else if (token == "BNB_DSATUR") {
         config = BNB_DSATUR;
     } else if (token == "BNB_DSATUR_SEWELL") {
         config = BNB_DSATUR_SEWELL;
-        
+    } else if (token == "BNB_DSATUR_PASS") {
+        config = BNB_DSATUR_PASS;
+
     } else {
         in.setstate(std::ios_base::failbit);
     }
